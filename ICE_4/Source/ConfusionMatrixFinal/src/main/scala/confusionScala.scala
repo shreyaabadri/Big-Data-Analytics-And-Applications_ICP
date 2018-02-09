fa43b5b85bda7cb3ed17d0a79a0a07670e0b68ad
khushbukolhe <31299412+khushbukolhe@users.noreply.github.com>
2018-02-09 17:26:43 -0600

    Uploading source code for ICE 4
    Uploading  source code for ICE4

---
+++ ICE_4/Source/ConfusionMatrixFinal/src/main/scala/confusionScala.scala
@@ -0,0 +1,70 @@
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+
+object confusionScala {
+
+  def main(args: Array[String]) {
+
+
+    val sparkConf = new SparkConf().setAppName("confusionScala").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+
+
+    val data = sc.textFile("/Volumes/Data/BigDataAnalytics/ICMP4/CS5542-Tutorial2B-SourceCode/image_classification_Linux_MacOS/input/sample.txt")
+    val parsedData = data.map(line=>{line.split(",")}).map(line=>(line(0),line(1)))
+    parsedData.foreach(println)
+
+    val predictionAndLabel =  parsedData.map(x => (if (x._1.equals("man"))
+      "0".toDouble
+    else  "1".toDouble,
+
+      if (x._2.equals("man"))
+        "0".toDouble
+      else "1".toDouble))
+
+
+    val confusion_metrics = new MulticlassMetrics(predictionAndLabel)
+    val confusionMatrix = confusion_metrics.confusionMatrix
+    println("Confusion Matrix:\n" )
+    println(confusionMatrix)
+
+    val tp = confusionMatrix(0, 0)   // True positive
+    val fn = confusionMatrix(0, 1)   // False negative
+    val fp = confusionMatrix(1, 0)   // False positive
+    val tn = confusionMatrix(1, 1)   // True negative
+    val total = tp+fn+fp+tn
+
+    //Accuracy=(TP+TN)/total
+    val accuracy = ((tp+tn)/total).toFloat
+    println("\nAccuracy :" + accuracy )
+
+    //Misclassification Rate=(FP+FN)/total
+    val miscRate = ((fp+fn)/total).toFloat
+    println("Misclassification Rate :" + miscRate )
+
+    // True Positive Rate=TP/actual yes
+    val truePositiveRate = (tp/(tp+fn)).toFloat
+    println("True Positive Rate :" + truePositiveRate)
+
+    //False Positive Rate:FP/actual no
+    val falsePositiveRate = (fp/(fp+tn)).toFloat
+    println("false Positive Rate :" + falsePositiveRate)
+
+    //Specificity=TN/actual no
+    val specificity = (tn/(fp+tn)).toFloat
+    println("Sensitivity :" + specificity)
+
+    //Precision=TP/predicted yes
+    val precision = (tp/(fp+tp)).toFloat
+    println("Precision :" + precision)
+
+    //Prevalence=actual yes/total
+    val prevalence = (tp+fn/(total)).toFloat
+    println("Precision :" + prevalence)
+
+
+
+  }
+
+}
